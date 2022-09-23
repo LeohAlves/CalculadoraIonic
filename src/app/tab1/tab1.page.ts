d914f6de1800b3a2197f37c7171b81742031d861
Leonardo Alves <leonardoalves72@hotmail.com>
2022-09-23 20:38:07 -0300

    commit inicial

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  valor1: number;
+  valor2: number;
+  resultado: Number;
+
+  constructor(private alertController: AlertController) { }
+
+
+  async mostrarAlertSoma() {
+    const alert = await this.alertController.create({
+      header: 'Resultado',
+      message: 'Valor 1:' + this.valor1 + 
+      '<br> Valor 2:' + this.valor2 +
+      '<br> Resultado: ' + (this.resultado = this.valor1 + this.valor2),
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+  async mostrarAlertSub() {
+    const alert = await this.alertController.create({
+      header: 'Resultado',
+      message: 'Valor 1: ' + this.valor1 + 
+      '<br> Valor 2: ' + this.valor2 + 
+      '<br> Resultado: ' + (this.resultado = this.valor1 - this.valor2),
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+  async mostrarAlertMult() {
+    const alert = await this.alertController.create({
+      header: 'Resultado',
+      message: 'Valor 1: ' + this.valor1 + 
+      '<br> Valor 2: ' + this.valor2 + 
+      '<br> Resultado: ' + (this.resultado = this.valor1 * this.valor2),
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+  async mostrarAlertDiv() {
+    const alert = await this.alertController.create({
+      header: 'Resultado',
+      message: 'Valor 1: ' + this.valor1 + 
+      '<br> Valor 2: ' + this.valor2 + 
+      '<br> Resultado: ' + (this.resultado = this.valor1 / this.valor2),
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+  async mostrarAlertMaior() {
+    const alert = await this.alertController.create({
+      header: 'Resultado',
+      message: '',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+  async mostrarAlertMenor() {
+    const alert = await this.alertController.create({
+      header: 'Resultado',
+      message: 'This is an alert!',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+}
