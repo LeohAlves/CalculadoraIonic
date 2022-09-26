a6f9c27aaf9454e1ddf3e1ebcde99c893e7e3e97
Leonardo Alves <leonardoalves72@hotmail.com>
2022-09-26 15:29:11 -0300

    alteraçoes nos alert

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,70 +11,88 @@ export class Tab1Page {
   valor1: number;
   valor2: number;
   resultado: Number;
-  maior: number;
   conta: string;
 
-  constructor(private alertController: AlertController) {}
-  
+  constructor(private alertController: AlertController) { }
+
+  limpar(){
+    this.valor1 = 0;
+    this.valor2 = 0;
+    this.resultado = 0;
+  }
 
   async mostrarAlertSoma() {
+    this.resultado = this.valor1 + this.valor2
+    this.conta = 'soma';
     const alert = await this.alertController.create({
-      
       header: 'Resultado',
-      message: 'Valor 1:' + this.valor1 +
-        '<br> Valor 2:' + this.valor2 +
-        '<br> Resultado: ' + (this.resultado = this.valor1 + this.valor2),
+      message: `<p>Valor 1: ${this.valor1}</p>
+                <p>Valor 2: ${this.valor2}</p>
+                <p>Resultado : ${this.resultado} </p>`,
       buttons: ['OK'],
     });
 
     await alert.present();
   }
   async mostrarAlertSub() {
+    this.conta = 'Subtraçao';
+    this.resultado = this.valor1 - this.valor2
     const alert = await this.alertController.create({
       header: 'Resultado',
-      message: 'Valor 1: ' + this.valor1 +
-        '<br> Valor 2: ' + this.valor2 +
-        '<br> Resultado: ' + (this.resultado = this.valor1 - this.valor2),
+      message: `<p>Valor 1: ${this.valor1}</p>
+                <p>Valor 2: ${this.valor2}</p>
+                <p>Resultado : ${this.resultado} </p>`,
       buttons: ['OK'],
     });
 
     await alert.present();
   }
   async mostrarAlertMult() {
+    this.conta = 'Multiplicaçao';
+    this.resultado = this.valor1 * this.valor2
     const alert = await this.alertController.create({
       header: 'Resultado',
-      message: 'Valor 1: ' + this.valor1 +
-        '<br> Valor 2: ' + this.valor2 +
-        '<br> Resultado: ' + (this.resultado = this.valor1 * this.valor2),
+      message:  `<p>Valor 1: ${this.valor1}</p>
+                  <p>Valor 2: ${this.valor2}</p>
+                  <p>Resultado : ${this.resultado} </p>`,
       buttons: ['OK'],
     });
 
     await alert.present();
   }
   async mostrarAlertDiv() {
+    this.resultado = this.valor1 / this.valor2
     const alert = await this.alertController.create({
       header: 'Resultado',
-      message: 'Valor 1: ' + this.valor1 +
-        '<br> Valor 2: ' + this.valor2 +
-        '<br> Resultado: ' + (this.resultado = this.valor1 / this.valor2),
+      message:  `<p>Valor 1: ${this.valor1}</p>
+                 <p>Valor 2: ${this.valor2}</p>
+                 <p>Resultado : ${this.resultado} </p>`,
       buttons: ['OK'],
     });
 
     await alert.present();
   }
   async mostrarAlertMaior() {
+    this.conta = 'Maior';
+    this.resultado = this.valor1 > this.valor2 ?this.valor1 : this.valor2; 
     const alert = await this.alertController.create({
       header: 'Resultado',
-      message: 'Maior: ' + this.maior,
+      message:  `<p>Valor 1: ${this.valor1}</p>
+                 <p>Valor 2: ${this.valor2}</p>
+                <p>Resultado : ${this.resultado} </p>`,
       buttons: ['OK'],
     });
 
     await alert.present();
   }
   async mostrarAlertMenor() {
+    this.conta = 'Menor numero';
+    this.resultado = this.valor1 < this.valor2 ? this.valor1 : this.valor2;
     const alert = await this.alertController.create({
       header: 'Resultado',
-      message: 'This is an alert!',
+      message:  `<p>Valor 1: ${this.valor1}</p>
+                 <p>Valor 2: ${this.valor2}</p>
+                <p>Resultado : ${this.resultado} </p>`,
       buttons: ['OK'],
     });
 
