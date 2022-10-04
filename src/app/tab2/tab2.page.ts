ed2e08a1138e0e274ff92298a2405b62a24053f7
Kaique <kaiqsouza2004@hotmail.com>
2022-10-03 21:05:33 -0300

    criando métodos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  operacao = '';
+  resultado = '';
 
   constructor() {}
+
+  ngOnInit() {}
+
+  adicionarValor(valor: any){
+ /* var numero = null;
+    var simbolo = null;
+
+    if (this.operacao != '*'){
+      numero = this.operacao;
+      if (this.operacao != simbolo){
+        this.operacao += numero;
+      }
+      
+    } else {
+      simbolo = this.operacao;
+      if (this.operacao != numero){
+        this.operacao += simbolo;
+      }
+    } */
+    this.operacao += valor;
+  }
+
+  limparMemoria(){
+    this.operacao = '';
+    this.resultado = '';
+
+  }
+
+  limparOperacao(){
+    this.operacao = '';
+
+  }
+
+  inverterValor(){
+
+  }
+
+  apagarCaracter() {
+    if (this.operacao.length > 0){
+      this.operacao = this.operacao.substring(0, this.operacao.length -1);
+    }
+  }
 
 }
