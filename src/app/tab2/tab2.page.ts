44c021ee6308a6630122373782ccc2b0761529d8
Kaique <kaiqsouza2004@hotmail.com>
2022-10-21 21:04:12 -0300

    criando metodo salvar operaçoes e resultados na memoria

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { evaluate } from 'mathjs';
+import { IMemoria } from '../models/IMemoria.model';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab2',
@@ -10,63 +13,118 @@ export class Tab2Page {
   operacao = '';
   resultado = '';
   numero = false;
-  caracter =true;
-  caracteres = ['-','*','/','+','.']
+  caracter = true;
+  caracteres = ['-', '*', '/', '+', '.']
+
+  memoria: IMemoria[] = [];
 
 
-  constructor() {}
+  constructor(private alertController: AlertController) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-  adicionarValor(valor: string){
+  adicionarValor(valor: string) {
     this.caracter = this.caracteres.includes(valor)
-    if (!this.caracter){
+    if (!this.caracter) {
       this.operacao += valor;
       this.numero = true;
-    }else if (this.caracter && this.numero){
+    } else if (this.caracter && this.numero) {
       this.operacao += valor;
       this.numero = false;
     }
   }
 
-  limparMemoria(){
+  limparMemoria() {
     this.operacao = '';
     this.resultado = '';
     this.numero = false;
 
   }
 
-  limparOperacao(){
+  limparOperacao() {
     this.operacao = '';
     this.numero = false;
 
   }
 
-  inverterValor(){
+  inverterValor() {
 
   }
 
   apagarCaracter() {
-    if (this.operacao.length > 0){
-      this.operacao = this.operacao.substring(0, this.operacao.length -1);
+    if (this.operacao.length > 0) {
+      this.operacao = this.operacao.substring(0, this.operacao.length - 1);
     }
 
-    const ultimo = this.operacao.substring(this.operacao.length,1);
+    const ultimo = this.operacao.substring(this.operacao.length, 1);
     this.caracter = this.caracteres.includes(ultimo);
     console.log(ultimo);
 
     if (!this.caracter) {
       this.numero = true;
-    }else {
+    } else {
       this.numero = false;
     }
   }
 
-  realizarOperacao(){
+  realizarOperacao() {
     try {
       this.resultado = evaluate(this.operacao);
     } catch (error) {
       this.resultado = 'Inválido!';
     }
   }
+
+  salvarMemoria() {
+    if (this.operacao != '' && this.resultado != '') {
+      const memoria: IMemoria = {
+        operacao: this.operacao,
+        resultado: Number(this.resultado)
+      };
+      this.memoria.push(memoria);
+    } else if (this.operacao != '' && this.resultado == '') {
+      this.realizarOperacao();
+
+      const memoria: IMemoria = {
+        operacao: this.operacao,
+        resultado: Number(this.resultado)
+
+      };
+      this.memoria.push(memoria);
+    } else {
+      this.presentAlert('Atenção!!!!', 'Digite uma operação!')
+    }
+    console.log(this.memoria);
+  }
+  async presentAlert(titulo: string, mensagem: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensagem,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  esvaziarMemoria(){
+
+  };
+
+  adicionarMemoria(){
+
+  };
+
+  removerMemoria(){
+
+  };
+  mostrarMemoria(){
+    if(this.memoria !=[]){
+      console.log(this.memoria);
+    } else{this.presentAlert('Atenção!!!!', 'Memória vazia')}
+
+  }
+  chamarMemoria(){}
+
+  
 }
+
